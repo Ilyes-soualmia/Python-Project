ff5d1bf11411ab570eac8ce75493a2e5995b67b0
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2025-01-01 20:37:14 +0100

    phase 1 done

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import json
 import qcm_handler as qcm
+import user_data_handler as udh
 
 # Main function
-
 data = qcm.load_data()
-name = qcm.welcome()
-subject = qcm.choose_subject()
-exam_name = qcm.choose_exam(data, subject)
-result = qcm.show_exam(data, subject, exam_name)
 
-#print result and correct answers
-print(f"Your result is: {result} / 10")
-for key, value in qcm.temp_dect.items():
-    print(f'{key}: {value}')
+username = udh.login()
+name = qcm.welcome()
+
+if username:
+    while True:
+        print("1. Take an exam")
+        print("2. View exam history")
+        print("3. Logout")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            subject = qcm.choose_subject()
+            exam_name = qcm.choose_exam(data, subject)
+            result = qcm.show_exam(data, subject, exam_name)
+            udh.record_exam_result(username, exam_name, result)
+            
+            #print result and correct answers
+            print(f"Your result is: {result} / 10")
+            for key, value in qcm.temp_dect.items():
+                if value:                       #if value is not empty 
+                    print(f'{key}: {value}')
+                else:                           #if value is empty, like it was the last question
+                    break
+
+        elif choice == '2':
+            udh.view_exam_history(username)
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+        
+    
+
+
+
    
